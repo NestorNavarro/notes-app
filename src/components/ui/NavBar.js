b49b70372f743c71fd61df9340361bed165f7808
NestorNavarro <nestor.navarroarizga@gmail.com>
2021-07-13 15:13:55 -0500

    navbar functionality

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../actions/auth';
 
 export const NavBar = () => {
+    const {user} = useSelector(state => state.auth);
+    const dispatch = useDispatch();
+    const handleLogout = () => {
+        dispatch( logout() );
+    }
     return (
         <ul className="notes__navbar">
             <li className="notes__navbar-content ml-5-own">
-                <i className="far fa-check-square"></i>
+                <i className="far fa-check-square"/>
             </li>
             <li className="notes__navbar-content">
-                <i className="far fa-comment-alt"></i>
+                <i className="far fa-comment-alt"/>
             </li>
             <li className="notes__navbar-content">
-                <i className="far fa-envelope"></i>
+                <i className="far fa-envelope"/>
             </li>
             <li className="notes__navbar-content">
-                <i className="far fa-calendar"></i>
+                <i className="far fa-calendar"/>
             </li>
             <li className="notes__navbar-content ">
-                <i className="far fa-star cl-star"></i>
+                <i className="far fa-star cl-star"/>
             </li>
             <li className="notes__navbar-content fl-r mr-5-own pointer-own">
-                <i className="fas fa-power-off btn-icon"></i>
+                <i 
+                    className="fas fa-power-off btn-icon"
+                    onClick={handleLogout}
+                />
             </li>
             <li className="notes__navbar-content fl-r">
-                <span >Nestor Eduardo Navarro Arizaga</span>
+                <span >{user?.name}</span>
             </li>
         </ul>
     );
